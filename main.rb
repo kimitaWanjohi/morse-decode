740d0bec77ae513a1f3a77bbd1fa553bc9a2f48d
Zuheb Ahmed <zuhebahmed101625@gmail.com>
2023-08-23 13:42:18 +0600

    Added logic for words

--- main.rb
+++ main.rb
@@ -18,3 +18,8 @@ end
 def decode_word(string)
   string.split('  ').map { |word| word.split.map { |char| decode_char(char) }.join }.join(' ')
 end
+
+def decode(string)
+  string.split('   ').map { |word| decode_word(word) }.join(' ')
+end
+p decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
